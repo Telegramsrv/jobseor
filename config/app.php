63d1409e9c851bd316e27b0defff8fc8fc71de03
TheArdent <oleksii.yaryi@gmail.com>
2017-02-21 02:55:45 +0200

    add packages: -laravel-debugbar - dev -laravel-ide-helper - dev -laravelcollective/html - prod -sleepingowl - prod

--- config/app.php
+++ config/app.php
@@ -177,6 +177,16 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        //prod
+        Collective\Html\HtmlServiceProvider::class,
+        SleepingOwl\Admin\Providers\SleepingOwlServiceProvider::class,
+
+        //dev
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+
+
     ],
 
     /*
@@ -225,6 +235,14 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+
+        //prod
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+
+        //dev
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
