b34e91f2fef55ea1504fb85c2dc589bdef31590f
TheArdent <oleksii.yaryi@gmail.com>
2017-02-24 20:23:08 +0200

    update view with using laravelcollective Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Login</div>
+                    <div class="panel-body">
+
+                        {!! Form::open(['route' => 'login', 'method' => 'post']) !!}
+
+                        {!! Form::token() !!}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                {!! Form::email('email', old('email'), [ 'class' =>'form-control', 'id' => 'email', 'required', 'autofocus']) !!}
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -28,7 +30,7 @@
                             <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                {!! Form::password('password',[ 'class' => 'form-control', 'id' => 'password', 'required']) !!}
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -42,7 +44,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                        {!! Form::checkbox('remember', 1,old('remember') ? 'checked' : '') !!}  Remember Me
                                     </label>
                                 </div>
                             </div>
@@ -50,19 +52,17 @@
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
+                                {!! Form::submit('Login',[ 'class' => 'btn btn-primary']) !!}
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     Forgot Your Password?
                                 </a>
                             </div>
                         </div>
-                    </form>
+                        {!! Form::close() !!}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
