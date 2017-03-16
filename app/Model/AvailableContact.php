eae656762321acbf348df4e6833aa52c134caded
TheArdent <oleksii.yaryi@gmail.com>
2017-03-16 12:41:19 +0200

    updated method isAvailable add timer 24*60*60

--- app/Model/AvailableContact.php
+++ app/Model/AvailableContact.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Model\AvailableContact
  *
- * @property int $id
- * @property int $owner_id
- * @property int $user_id
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
+ * @property int                  $id
+ * @property int                  $owner_id
+ * @property int                  $user_id
+ * @property \Carbon\Carbon       $created_at
+ * @property \Carbon\Carbon       $updated_at
  * @property-read \App\Model\User $owner
  * @property-read \App\Model\User $user
  * @method static \Illuminate\Database\Query\Builder|\App\Model\AvailableContact whereCreatedAt($value)
@@ -23,27 +23,27 @@ use Illuminate\Database\Eloquent\Model;
  */
 class AvailableContact extends Model
 {
-    public function owner()
-    {
-    	return $this->belongsTo('App\Model\User', 'owner_id');
-    }
+	public function owner()
+	{
+		return $this->belongsTo('App\Model\User', 'owner_id');
+	}
 
-    public function user()
-    {
-    	return $this->belongsTo('App\Model\User', 'user_id');
-    }
+	public function user()
+	{
+		return $this->belongsTo('App\Model\User', 'user_id');
+	}
 
-    public function isAvailable($first_user,$second_user)
-    {
-	    $availableContact = AvailableContact::whereUserId($first_user)->whereOwnerId($second_user)
-	                                        ->orWhere('user_id', $second_user)->whereOwnerId($first_user)
-	                                        ->get();//TODO Add date
-
-	    if ($availableContact->isEmpty()) {
-		    return false;
-	    }
-	    else {
-	        return true;
-	    }
-    }
+	public function isAvailable($first_user, $second_user)
+	{
+		$availableContact = AvailableContact::whereUserId($first_user)->whereOwnerId($second_user)
+		                                    ->orWhere('user_id', $second_user)->whereOwnerId($first_user)
+			> whereDate('created_at', '>', date('Y-m-d H:m:s', time() - 24 * 60 * 60))
+				->get();
+		if ($availableContact->isEmpty()) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 }
