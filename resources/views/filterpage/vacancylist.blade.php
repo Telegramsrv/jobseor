f5223666a58a3832c9176885068584dff98e8c76
TheArdent <oleksii.yaryi@gmail.com>
2017-03-17 13:43:34 +0200

    updated view vacancy list at filter page

--- resources/views/filterpage/vacancylist.blade.php
+++ resources/views/filterpage/vacancylist.blade.php
@@ -1,26 +1,24 @@
 <div class="container">
     <div class="row" style="margin-left: 0; margin-right: 0;">
         <div class="headervakanse">
-            Найдено: 2 резюме. <span style="float: right;">За последние 30 дней</span>
+            Найдено: {{ $vacancies->count() }} резюме.
         </div>
+
+        @foreach($vacancies as $vacancy)
+
         <div class="vakanceblock">
             <div class="avatarvacanse">
-                <img src="/img/nophoto.png" alt="Аватар без фото" title="name resume">
+                <img src="/{{ $vacancy->user->image }}" alt="Аватар без фото" title="name resume">
             </div>
-            <h2><a href="/rezume-blank.php">Web developer</a></h2>
-            <p>Иванов Иван , 32 года, Киев</p>
-            <p>Полная занятость. Опыт работы от 2 лет. Среднее специальное образование.
-                Баядера Групп • крупный национальный холдинг, специализирующийся на производстве и реализации высококачественной…</p>
+            <h2><a href="{{ route('vacancy.view', [ 'id' => $vacancy->vacancy_id]) }}">{{ $vacancy->title }}</a></h2>
+            <p>{{ $vacancy->user->name }} , {{ $vacancy->country->name }}, {{ $vacancy->city }}</p>
+            <p>{{ $vacancy->employment->name }} занятость. Опыт работы
+                : {{ $vacancy->experience_type->name }}. {{ $vacancy->education->name }} образование.
+                • {{ str_limit($vacancy->description, 97) }}…</p>
         </div>
-        <div class="vakanceblock">
-            <div class="avatarvacanse">
-                <img src="/img/nophoto.png" alt="Аватар без фото" title="name resume">
-            </div>
-            <h2><a href="/rezume-blank.php">Mobile Developer</a></h2>
-            <p>Seor Company , Киев, 2 ч. назад</p>
-            <p>Полная занятость. Опыт работы от 2 лет. Среднее специальное образование.
-                Баядера Групп • крупный национальный холдинг, специализирующийся на производстве и реализации высококачественной…</p>
-        </div>
+
+        @endforeach
+
         <div class="col-md-12">
         </div>
     </div>
